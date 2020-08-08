8fb09ec9ce2c4c967312ee210d6ba7fe800b37de
LAPTOP-0FRK4RU6\rubyr <rubyruby1010@naver.com>
2020-08-08 21:23:54 +0900

    add another example

--- nicecode vs badcode/getpushmessage.js
+++ nicecode vs badcode/getpushmessage.js
@@ -22,3 +22,49 @@ function getPushMessage(order) {
 
     return messages[order.type];
 }
+
+//Example
+
+
+/**
+ * @param {string} host
+ * @param {string} user
+ * @param {Object[]} booking
+ * @param {string} booking[].status
+ */
+const getStatus = (host, user, booking) => {
+    if (booking.status === "pending") {
+        return `Hey ${user}, we're awaiting confirmation from ${host}`;
+    } else if (booking.status === "confirmed") {
+        return `Hey ${user}, ${host} is excited to be hosting you!`;
+    } else if (booking.status === "canceled") {
+        return `Unfortunately ${user}, ${host} has canceled your booking request`;
+    } else if (booking.status === "done") {
+        return `${host} hopes you had a great stay!`;
+    }
+}
+
+
+const getStatus = (host, user, booking) => {
+    const messages = {
+        "pending": `Hey ${user}, we're awaiting confirmation from ${host}`
+        , "confirmed": `Hey ${user}, ${host} is excited to be hosting you!`
+        , "canceled": `Unfortunately ${user}, ${host} has canceled your booking request`
+        , "done": `${host} hopes you had a great stay!`
+    };
+
+    return messages[booking.status];
+
+}
+//sample usage
+
+
+const booking1 = {
+    status: "pending"
+};
+console.log(getStatus("John", "Blane", booking1));
+const booking2 = {
+    status: "confirmed"
+}
+console.log(getStatus("John", "Blane", booking2));
+
